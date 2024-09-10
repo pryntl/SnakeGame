9ac1a3ff09f46956280fd0e5341a808849937735
pryntl <pryntl2007@gmail.com>
2024-09-10 18:08:32 +0330

    fix js
    improved the styles.

--- secondPage/second.js
+++ secondPage/second.js
@@ -2,9 +2,9 @@
 document.addEventListener("keydown", function (e) {
     if (e.code == 'Space'){
         let text = document.getElementById('text')
-        text.textContent = 'Have Fun!Click Here To Start The Game.'
+        text.textContent = 'Have Fun! Click Here To Start The Game.'
         text.style.color = 'pink'
-        text.style.textShadow = '0px 0px 10px white'
+        text.style.textShadow = '0px 0px 15px white'
         text.style.textDecoration ='none'
         text.href = 'gamePage/game.html'
     }
